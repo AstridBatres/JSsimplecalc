2ddd30b9960060a94259dbaf9a82e42268e49ba6
AstridBatres-Guerrero <astridcbg28@gmail.com>
2022-04-06 18:53:11 -0700

    Java-class 3
    Calculations

---
+++ script.js
@@ -0,0 +1,30 @@
+//function expression
+// function greet(name){
+//  return "Hello" + name + "!";
+// }
+
+//console.log(greet)
+
+
+
+//let greet = function(name)
+//{
+  //  return "Hello" + name + "!";
+
+//}
+
+//console.log(greet("Eric"));
+
+//function displayName(studentName){
+  //  document.write(`
+      //  <h2>Student List</h2>
+      //  <p> Student name: ${studentName}</p>
+     //   <hr>
+  // `)
+//}
+
+//function multiplyThreewhich (num1)
+//return num1 * 3;
+//document.write(`
+//`)
+
