f9153d5d6560c20e0c8715db95d8068745b6fb83
AstridBatres-Guerrero <astridcbg28@gmail.com>
2022-04-08 21:03:38 -0700

    Simple Cal-Assignment 3
    Added rest of operations

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
 function calculator(){
     console.log("Calculating...");
     let num1 = Number (prompt("Enter the number"));
+    let num2 = Number (prompt("Enter the number"));
     document.getElementById("calculationZone").innerHTML=`
-    <p>${num1} * 10 = ${num1 * 10}`;
+     <p>${num1} + ${num2} = ${num1 + num2} </p>
+     <p>${num1} - ${num2} = ${num1 -num2}</p>
+     <p> ${num1} / ${num2} = ${num1/num2}</p>
+     <p>${num1} * ${num2} = ${num1 * num2}</p>`;
 }
